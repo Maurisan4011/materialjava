41ccf16a21d01626b5a01e3945a010a9c406722b
Mauricio <maurisan4011@gmail.com>
2020-12-04 11:06:33 -0300

    Se agrega Swagger de clase 34 a proyecto

--- MaterialJavaFullStackCaC2011/SoloJava/proyecto-springboot-cac/src/main/java/com/maudev/springboot/app/controller/AppController.java
+++ MaterialJavaFullStackCaC2011/SoloJava/proyecto-springboot-cac/src/main/java/com/maudev/springboot/app/controller/AppController.java
@@ -1,18 +1,23 @@
 package com.maudev.springboot.app.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.maudev.springboot.app.models.Perro;
 import com.maudev.springboot.app.models.Persona;
 
-@Controller
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
 public class AppController {
 
 //	PRIMER ENDPOINT O REQUEST ONTIENE  POR STRING TAGS DE HTML
-	@RequestMapping("/obtenerMagic")
-	@ResponseBody
+
+	@Operation(summary = "Obtiene home o Magic", description = "" + "con este servicio obtenemos Magic"
+			+ "alumnes codo a codo manijas")
+	@RequestMapping(value = "/obtenerMagic", method = RequestMethod.GET)
 	String home() {
 		return "<h1>Buenas noches Equipo CaC JavaFullStack a romperla</h1> " + "<h2>con Spring Boot</h2>"
 				+ "<iframe src=\"https://giphy.com/embed/12NUbkX6p4xOO4\" width=\"120\" height=\"120\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/shia-labeouf-12NUbkX6p4xOO4\">via GIPHY</a></p>";
@@ -20,17 +25,21 @@ public class AppController {
 
 //	SEGUNDO ENDPOINT O REQUEST
 //	OBTIENE LOS DATOS DE LA PERSONA INSTANCIADA
-	@RequestMapping("/obtenerPersona")
-	@ResponseBody
+	@Operation(summary = "Obtener Persona", description = "" + "con este servicio se obtiene los datos de una persona "
+			+ "alumnes codo a codo manijas")
+
+	@RequestMapping(value = "/obtenerPersona", method = RequestMethod.GET)
 	String persona() {
 		Persona persona1 = new Persona("hector", "orbegoso", 20);
 
 		return persona1.toString();
 	}
 
-//	TERCE  ENDPOINT O REQUEST
+//	TERCER  ENDPOINT O REQUEST
 //	OBTIENE LOS DATOS DEL ONJETO PERRO INSTANCIADO
-	@RequestMapping("/obtenerPerro")
+	@Operation(summary = "Obtener Perro", description = "" + "con este servicio se obtiene los datos de un "
+			+ "alumnes codo a codo manijas")
+	@RequestMapping(value = "/obtenerPerro", method = RequestMethod.GET)
 	@ResponseBody
 	String perro() {
 
